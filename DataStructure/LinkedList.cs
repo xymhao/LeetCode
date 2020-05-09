19be00b357038215021b0e2466c9aff6c4573a56
许玥明 <xuyueming@beisen.com>
2020-05-09 13:14:46 +0800

    链表结构

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    public class LinkedList
+    {
+        public static LinkedNode GetLinkedList(int[] values)
+        {
+            var flag = new LinkedNode(0);
+            if (values.Length  <=0 )
+            {
+                return null;
+            }
+            var curNode = new LinkedNode(values[0]);
+            flag.Next = curNode;
+            for (var index = 1; index < values.Length; index++)
+            {
+                int val = values[index];
+                curNode.Next = new LinkedNode(val);
+                curNode = curNode.Next;
+            }
+
+            return flag.Next;
+        }
+    }
+
+    public class LinkedNode
+    {
+        public object Data { get; set; }
+
+        public LinkedNode Next { get; set; }
+
+        public LinkedNode(object data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ DataStructureTests/LinkedListTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructure;
+using NUnit.Framework;
+
+namespace DataStructureTests
+{
+    
+    public class LinkedListTests
+    {
+        [Test]
+        public void Should_Get_Link_List()
+        {
+            //todo 构造一个 1->2->3的链表并返回表头
+            LinkedNode result = LinkedList.GetLinkedList(new[] {1, 2, 3, 4, 5});
+
+            Assert.AreEqual(1,result.Data);
+            Assert.AreEqual(2,result.Next.Data);
+            Assert.AreEqual(3,result.Next.Next.Data);
+            Assert.AreEqual(4, result.Next.Next.Next.Data);
+            Assert.AreEqual(5, result.Next.Next.Next.Next.Data);
+
+        }
+    }
+}
