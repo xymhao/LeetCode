3e6741f58851dfa6529b770c60db2bb0d6edd1ef
xuyueming <xnysym@outlook.com>
2020-05-10 09:22:32 +0800

    冒泡排序

---
+++ DataStructure/BubbleSort/Sort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.BubbleSort
+{
+    public class Sort
+    {
+        public static int[] BubbleSort(int[] array)
+        {
+            //冒泡排序需要循环array.length次
+            for (int j = 0; j < array.Length; j++)
+            {
+                bool flag = false;
+                //从0开始，如果i+1 > i， 则交换位置
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    var temp = array[i];
+                    if (temp > array[i + 1])
+                    {
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        flag = true;
+                    }
+                }
+
+                if (flag == false)
+                {
+                    break;
+                }
+            }
+            return array;
+        }
+
+
+    }
+}
---
+++ DataStructureTests/SortTests.cs
@@ -0,0 +1,22 @@
+﻿using DataStructure.BubbleSort;
+using NUnit.Framework;
+
+namespace DataStructureTests
+{
+    [TestFixture]
+    public class SortTests
+    {
+        [Test]
+        public void BubbleSortTests()
+        {
+            var array = new int[] { 4, 5, 6, 3, 2, 1 };
+            Sort.BubbleSort(array);
+            Assert.AreEqual(1,array[0]);
+            Assert.AreEqual(2,array[1]);
+            Assert.AreEqual(3,array[2]);
+            Assert.AreEqual(4,array[3]);
+            Assert.AreEqual(5,array[4]);
+            Assert.AreEqual(6,array[5]);
+        }
+    }
+}
