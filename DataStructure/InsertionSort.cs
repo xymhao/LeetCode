fb93b1bf60f98796d0d260d49b57c51002979c3d
yueming xu <xnysym@outlook.com>
2021-01-05 22:22:02 +0800

    插入排序。

---
+++ DataStructure/InsertionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DataStructure
+{
+    public class InsertionSort
+    {
+        public static void Sort(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                var value = nums[i];
+                int j = i - 1;
+                for (; j >= 0; j--)
+                {
+                    if (nums[j] > value)
+                    {
+                        nums[j + 1] = nums[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                nums[j + 1] = value;
+            }
+        }
+    }
+}
---
+++ DataStructureTests/InsertionSortTests.cs
@@ -0,0 +1,27 @@
+﻿using DataStructure;
+using NUnit.Framework;
+
+namespace DataStructureTests
+{
+    [TestFixture]
+    public class InsertionSortTests
+    {
+        [Test]
+        public void Should_Sort()
+        {
+            var array = new int[] { 4, 6, 3, 2, 1, 5 };
+            InsertionSort.Sort(array);
+            AssertSort(array);
+        }
+        
+        private static void AssertSort(int[] array)
+        {
+            Assert.AreEqual(1, array[0]);
+            Assert.AreEqual(2, array[1]);
+            Assert.AreEqual(3, array[2]);
+            Assert.AreEqual(4, array[3]);
+            Assert.AreEqual(5, array[4]);
+            Assert.AreEqual(6, array[5]);
+        }
+    }
+}
