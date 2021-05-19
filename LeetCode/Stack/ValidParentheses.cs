9a8cc4adfc0c41de8384e0308d90f9968f83aaff
yueming xu <xnysym@outlook.com>
2021-05-19 17:41:23 +0800

    有效的括号匹配。

---
+++ LeetCode/Stack/ValidParentheses.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode.Stack
+{
+    /// <summary>
+    /// 有效的括号匹配
+    /// </summary>
+    public class ValidParentheses
+    {
+        /*
+         * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+         */
+
+        public bool IsValid(string s)
+        {
+            if (s == null || s.Length % 2 == 1)
+            {
+                return false;
+            }
+            var stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(c);
+                    continue;
+                }
+                
+                char leftP = stack.Peek();
+                if (leftP == '(' && c == ')'
+                    || leftP == '[' && c == ']'
+                    || leftP == '{' && c == '}')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ LeetCodeTests/Stack/ValidParenthesesTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Stack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Stack
+{
+    [TestClass]
+    public class ValidParenthesesTests
+    {
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("()", true)]
+        [DataRow("()[]{}", true)]
+        [DataRow("{[]}", true)]
+        [DataRow("(]", false)]
+        [DataRow("([)]", false)]
+        [DataRow("]", false)]
+        public void Should_IsValid(string s, bool expect)
+        {
+            var result = new ValidParentheses().IsValid(s);
+            
+            Assert.AreEqual(expect, result);
+        }
+    }
+}
