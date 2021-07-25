effa30c8cf00a543b8f9566110d76d2628166e86
yueming xu <xnysym@outlook.com>
2021-07-25 17:34:11 +0800

    二进制数求和。

---
+++ DesignPatternsTests/Demo/IPerson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DesignPatterns.Tests.Demo
+{
+    public interface IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+        void Introduction();
+        string GetName();
+    }
+
+    public class Student : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+        
+        public void Introduction()
+        {
+            Console.WriteLine("I am teacher:" + Name);
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ LeetCodeTests/AddBinaryTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public class AddBinaryTests
+    {
+        [TestMethod]
+        public void Should_Result_11_add_1()
+        {
+            Assert.AreEqual("100", AddBinary("11", "1"));
+            //"10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101"
+            //"110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
+            Assert.AreEqual("100",
+                AddBinary(
+                    "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+                    "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
+        }
+
+        private string AddBinary(string a, string b)
+        {
+            var ans = new List<char>();
+
+            int n = Math.Max(a.Length, b.Length);
+            var carry = false;
+            for (int i = 0; i < n; ++i)
+            {
+                var sum = 0;
+                sum += i < a.Length ? (a.ToCharArray()[a.Length - 1 - i] - '0') : 0;
+                sum += i < b.Length ? (b.ToCharArray()[b.Length - 1 - i] - '0') : 0;
+                if (carry)
+                {
+                    sum++;
+                }
+
+                ans.Add((char) (sum % 2 + '0'));
+                carry = sum >= 2;
+            }
+
+            if (carry)
+            {
+                ans.Add('1');
+            }
+
+            ans.Reverse();
+            return new string(ans.ToArray());
+        }
+
+        /// <summary>
+        /// 精度不够
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string AddBinary2(string a, string b)
+        {
+            var x = Convert.ToInt64(a, 2);
+            var y = Convert.ToInt64(b, 2);
+            ;
+
+            while (y != 0)
+            {
+                var answer = x ^ y;
+                var carry = (x & y) << 1;
+                x = answer;
+                y = carry;
+            }
+
+            return Convert.ToString(x, 2);
+        }
+    }
+}
